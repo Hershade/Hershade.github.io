26422b76c26852a87821c2734d7604f144a8a7f0
Hershade <germanxd717@gmail.com>
2023-01-04 11:39:05 -0600

    Add overlay to the page and functions to the menu

--- js/app.js
+++ js/app.js
@@ -16,6 +16,24 @@ const abrirMenu = () =>{
 
 const botonCerrar = () =>{
     const btnCerrar = document.createElement('p');
+    const overlay = document.createElement('div');
+    overlay.classList.add('pantalla-completa');
+    const body = document.querySelector('body')
+    body.appendChild(overlay);
     btnCerrar.textContent = 'x';
     btnCerrar.classList.add('btn-cerrar');
+    navegacion.appendChild(btnCerrar)
+    cerrarMenu(btnCerrar, overlay);
+}
+const cerrarMenu = (boton, overlay) =>{
+    boton.addEventListener('click',()=>{
+        navegacion.classList.add('ocultar');
+        overlay.remove();
+    });
+
+    /*Cuando le usuario haga click en el overlay se quitara el overlay y se cerrara el menu */
+    overlay.onclick = function(){
+        overlay.remove();
+        navegacion.classList.add('ocultar');
+    }
 }
